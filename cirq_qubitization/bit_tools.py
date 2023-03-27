f7159731ef75ed4c90a69bc1297a4bb8dbf33046
Fionn Malone <fmalone@google.com>
2023-03-27 20:58:06 +0000

    Floating point utilities and qrom updates.

--- cirq_qubitization/bit_tools.py
+++ cirq_qubitization/bit_tools.py
@@ -1,4 +1,6 @@
 from typing import Iterator
+import math
+from unicodedata import digit
 
 
 def iter_bits(val: int, width: int) -> Iterator[int]:
@@ -15,3 +17,25 @@ def iter_bits(val: int, width: int) -> Iterator[int]:
         raise ValueError(f"{val} exceeds width {width}.")
     for b in f'{val:0{width}b}':
         yield int(b)
+
+
+def float_to_fixed_point_bits(val: float, width: int, num_fractional_bits: int) -> str:
+    scale_factor = 2**num_fractional_bits
+    scaled_val = math.floor(abs(val * scale_factor))
+    fixed_point = bin(scaled_val)[2:2+width-1]
+    if len(fixed_point) < width:
+        fixed_point = '0'*(width-len(fixed_point)) + fixed_point
+    if val < 0:
+        return '1' + fixed_point
+    else:
+        return '0' + fixed_point
+
+def fixed_point_bits_to_float(bits: str, num_fractional_bits) -> float:
+    sign = (-1)**(int(bits[0]))
+    number = 0.0
+    integer_width = len(bits) - num_fractional_bits - 1
+    for indx, bit in enumerate(bits[1:]):
+        power = (integer_width - indx-1)
+        if int(bit):
+            number += 2**power
+    return sign * number
--- cirq_qubitization/bit_tools_test.py
+++ cirq_qubitization/bit_tools_test.py
@@ -1,6 +1,10 @@
 import pytest
 
-from cirq_qubitization.bit_tools import iter_bits
+from cirq_qubitization.bit_tools import (
+    iter_bits,
+    fixed_point_bits_to_float,
+    float_to_fixed_point_bits,
+)
 
 
 def test_iter_bits():
@@ -10,3 +14,22 @@ def test_iter_bits():
     assert list(iter_bits(3, 2)) == [1, 1]
     with pytest.raises(ValueError):
         assert list(iter_bits(4, 2)) == [1, 0, 0]
+
+
+@pytest.mark.parametrize(
+    "input,expected", [[('1101001', 2), -10.25], [('010101010101', 4), 85.3125]]
+)
+def test_fixed_point_bits_to_float(input, expected):
+    bin_val, frac = input
+    val_fixed = fixed_point_bits_to_float(bin_val, frac)
+    assert val_fixed == expected
+
+
+
+@pytest.mark.parametrize(
+    "input,expected", [[(-10.39499592010, 8, 2), '1101001'], [(2**8 / 3.0, 12, 4), '010101010101']]
+)
+def test_float_to_fixed_point_bits(input, expected):
+    val, width, frac = input
+    bin_val = float_to_fixed_point_bits(val, width, frac)
+    assert bin_val == expected
